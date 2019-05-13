b33ef176e407fae8e53e036d87e96e5518950aaf
Lyle Scott, III <lyle@ls3.io>
2019-05-13 17:48:02 -0400

    typo

--- src/gooee_statsd/__init__.py
+++ src/gooee_statsd/__init__.py
@@ -30,7 +30,7 @@ class StatsDClient(DogStatsd):
     def timed(self, *args, **kwargs):
         super().timed(*args, **kwargs)
 
-    # Make the api act like the "core" statsd API so we can switch of datadog easily if needed.
+    # Make the api act like the "core" statsd API so we can switch off datadog easily if needed.
     incr = DogStatsd.increment
     decr = DogStatsd.decrement
     timer = DogStatsd.timed
