6d1b10adeb9837f6dc081e3fc0f40a0b347d82c5
Lyle Scott III <lyle@ls3.io>
2019-05-20 10:37:37 -0400

    Updates for abstracting more client boiler plate into the lib (#3)

--- src/gooee_statsd/__init__.py
+++ src/gooee_statsd/__init__.py
@@ -1,10 +1,11 @@
 import logging
 import time
+from typing import Iterable
 
 from datadog import api
 from datadog.util.hostname import get_hostname
 
-from gooee_statsd.client import StatsDClient, statsd
+from gooee_statsd.client import statsd
 from gooee_statsd.decorators import statsd_enabled
 
 name = 'gooee_statsd'
@@ -14,13 +15,19 @@ LOG = logging.getLogger('gooee_statsd')
 
 @statsd_enabled
 def initialize(
-        host: str,
-        namespace: str,
-        port: int = 8125,
-        host_name: str = None,
-        use_default_route: bool = False
+    env_name: str,
+    host: str,
+    namespace: str,
+    port: int = 8125,
+    host_name: str = None,
+    use_default_route: bool = False,
+    constant_tags: Iterable[str] = None,
 ) -> None:
-    """Initialize a statsd client."""
+    """
+    Initialize a statsd client.
+
+    NOTE: `host` is the statsd host and `hostname` is the hostname to report as a tag.
+    """
     LOG.debug('Initializing Gooee StatsD lib with host={host} port={port}')
     # Monkeypatch the datadog statsd lib so that the "defaults" are auto supplied.
     api._host_name = host_name if host_name is not None else get_hostname()
@@ -28,13 +35,19 @@ def initialize(
     statsd.host = statsd.resolve_host(host, use_default_route)
     statsd.port = port
     statsd.namespace = namespace
+    statsd.constant_tags = [f'env:{env_name}', f'app_name:{namespace}'] + (constant_tags or [])
 
 
-def timer_helper(name: str, t0: float) -> None:
+def timer_helper(
+    name: str,
+    t0: float,
+    tags: Iterable[str] = None,
+    sample_rate: int = 1,
+) -> None:
     """
     Abstract how we do timings so the caller just needs to supply a name and initial time.
 
-    t0 should be a timer.perf_counter()
+    t0 should be the value of timer.perf_counter()
     """
-    ms = int((time.perf_counter() - t0) * 1000)
-    statsd.timer(name, ms)
+    ms = int(time.perf_counter() - t0) * 1000
+    statsd.timing(name, ms, tags=tags, sample_rate=sample_rate)
--- src/gooee_statsd/decorators.py
+++ src/gooee_statsd/decorators.py
@@ -1,10 +1,14 @@
+import logging
 import os
+
+LOG = logging.getLogger('gooee_statsd')
 
 
 def statsd_enabled(func):
     """Short circuit a function if statsd is not enabled."""
     def function_wrapper(*args, **kwargs):
         if not os.getenv('STATSD_ENABLED'):
+            LOG.debug(f'STATSD_ENABLED is not set so skipping statsd operation: {args} {kwargs}')
             return
         func(*args, **kwargs)
     return function_wrapper
