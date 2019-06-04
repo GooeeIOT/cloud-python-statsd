1eaabf4c173a28358d722e5a6e015771e0480ba0
Lyle Scott, III <lyle@ls3.io>
2019-06-04 15:29:47 -0700

    Update metric helpers

--- src/gooee_statsd/client.py
+++ src/gooee_statsd/client.py
@@ -17,10 +17,29 @@ class StatsDClient(DogStatsd):
     def timed(self, *args, **kwargs):
         super().timed(*args, **kwargs)
 
+    def _sanitize_metric(self, metric):
+        return metric.replace(':', '_')
+
+    def increment(self, metric, value=1, tags=None, *args, **kwargs):
+        metric = self._sanitize_metric(metric)
+        return super().increment(metric, value, tags, *args, **kwargs)
+
+    def decrement(self, metric, value=1, tags=None, *args, **kwargs):
+        metric = self._sanitize_metric(metric)
+        return super().decrement(metric, value, tags, *args, **kwargs)
+
+    def timed(self, metric=None, tags=None, *args, **kwargs):
+        metric = self._sanitize_metric(metric)
+        return super().timed(metric, tags, *args, **kwargs)
+
+    def timing(self, metric, value, tags=None, *args, **kwargs):
+        metric = self._sanitize_metric(metric)
+        return super().timing(metric, value, tags, *args, **kwargs)
+
     # Make the api act like the "core" statsd API so we can switch off datadog easily if needed.
-    incr = DogStatsd.increment
-    decr = DogStatsd.decrement
-    timer = DogStatsd.timed
+    incr = increment
+    decr = decrement
+    timer = timing
 
 
 # Create handle to statsd. This is what is used by consumer apps.
